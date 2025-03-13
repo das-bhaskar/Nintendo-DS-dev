6bcfaf1f5bd291d9d24d64a94dace88704cf6c61
aloofjelly <aloofjelly@gmail.com>
2025-03-13 13:25:05 -0400

    initial commit
    uploading currently worked on files. no guarantee of working as of yet

---
+++ trial2/template/source/keyinput.cpp
@@ -0,0 +1,73 @@
+#include "keyinput.h"
+#include <nds.h>
+#include <stdio.h>
+//#include </opt/devkitpro/libnds/include/nds/arm9/keyboard.h>
+
+void OnKeyPressed(int key)
+{
+    return;
+}
+// void InitKB()
+// {
+//     return Keyboard * keyboardDemoInit();
+// }
+int getKey()
+{
+    return keysCurrent();
+}
+int getKey(int size, int list[])
+{
+    return keysCurrent();
+}
+int getKey(int k1)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6, int k7)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6, int k7, int k8)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6, int k7, int k8, int k9)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6, int k7, int k8, int k9, int k10)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6, int k7, int k8, int k9, int k10, int k11)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6, int k7, int k8, int k9, int k10, int k11, int k12)
+{
+    return keysCurrent();
+}
+int getKey(int k1, int k2, int k3, int k4, int k5, int k6, int k7, int k8, int k9, int k10, int k11, int k12, int k13)
+{
+    return keysCurrent();
+}
